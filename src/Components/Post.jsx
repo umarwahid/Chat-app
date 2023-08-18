fa5cf93bce3bbcb8492b5fbe325bae1c8c723702
Umar Wahid <66332510+umarwahid@users.noreply.github.com>
2023-08-18 10:02:42 +0500

    some edit

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import PostCard from './PostCard'
+import React from 'react';
+import PostCard from './PostCard';
+import street2 from "./street.jpeg"
+import street3 from "./street2.jpeg"
+
 
 export default function Post() {
   return (
@@ -21,8 +24,8 @@ export default function Post() {
         </div>
       <PostCard name="Muhammad Umar" imgUrl="../myimage.jpg" content="image on beach" profileimg="../myimage.jpg"/>
       <PostCard name="Hafiz Saad Rizvi" imgUrl="https://i.pinimg.com/736x/97/df/27/97df27c310ac2d5a8c8cacd12b21c20e.jpg" content="TLP Leader" profileimg="https://i.pinimg.com/736x/97/df/27/97df27c310ac2d5a8c8cacd12b21c20e.jpg"/>
-      <PostCard name="Muhammad Umar" imgUrl="../myimage.jpg" content="image on beach" profileimg="../myimage.jpg"/>
-      <PostCard name="Muhammad Umar" imgUrl="../myimage.jpg" content="image on beach" profileimg="../myimage.jpg"/>
+      <PostCard name="Muhammad Umar" imgUrl={street2} content="image on beach" profileimg="../myimage.jpg"/>
+      <PostCard name="Muhammad Umar" imgUrl={street3} content="image on beach" profileimg="../myimage.jpg"/>
       </div>
   )
 }
